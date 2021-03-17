0f736563524eb4ec3cadd6d2031b825554e896db
Dikaver <dika1596@gmail.com>
2021-03-18 00:04:36 +0200

    Titan

--- Titan/Function/ODEFunction.java
+++ Titan/Function/ODEFunction.java
@@ -16,6 +16,8 @@ public class ODEFunction implements ODEFunctionInterface {
 
 	private final PlanetBody[] planets;
 
+	private boolean flag = true;
+
 	private final double massProbe;
 	public ODEFunction(PlanetBody[] planets, double massProbe){
 		this.planets = planets;
@@ -44,24 +46,29 @@ public class ODEFunction implements ODEFunctionInterface {
 
 		planets[11] = new PlanetBody(massProbe, y.position, y.velocity);
 
+		if(flag) {
+			flag = false;
+			initialMomenta();
+		}
+
 		LinkedList<Vector3d> positions = new LinkedList<>();
 
 		LinkedList<Vector3d> velocity = new LinkedList<>();
 
-		for(PlanetBody planet : planets) {
+		for(int i = 0; i < planets.length; i++) {
 
 			//TODO check Version 1
-            /*
-            double[] motion = motion_X_Y_Z(planet);
-            double[] force = forceMotion_X_Y_Z(planet);
+
+            double[] motion = motion_X_Y_Z(planets[i]);
+            double[] force = forceMotion_X_Y_Z(planets[i]);
             positions.add(new Vector3d(h * motion[0], h * motion[1], h * motion[2]));
             velocity.add(new Vector3d(h * force[0], h * force[1], h * force[2]));
-            */
-			//TODO check Version 2
-			double[] force = forceMotion_X_Y_Z(planet);
 
-			velocity.add(new Vector3d(h * (force[0]/planet.getM()), h * (force[1]/planet.getM()), h * (force[2]/planet.getM())));
-			positions.add(new Vector3d(h * planet.getVelocity().getX(), h * planet.getVelocity().getY(), h * planet.getVelocity().getZ()));
+			//TODO check Version 2
+//			double[] force = forceMotion_X_Y_Z(planets[i]);
+//
+//			velocity.add(new Vector3d(h * (force[0]/planets[i].getM()), h * (force[1]/planets[i].getM()), h * (force[2]/planets[i].getM())));
+//			positions.add(new Vector3d(h * planets[i].getVelocity().getX(), h * planets[i].getVelocity().getY(), h * planets[i].getVelocity().getZ()));
 		}
 
 
@@ -69,7 +76,6 @@ public class ODEFunction implements ODEFunctionInterface {
 		for(int i = 0; i < planets.length-1; i++){
 			planets[i].setPosition((Vector3d) planets[i].getPosition().add(positions.get(point)));
 			planets[i].setVelocity((Vector3d) planets[i].getVelocity().add(velocity.get(point)));
-			point++;
 		}
 		return new Rate(positions.getLast(), velocity.getLast());
 	}
@@ -78,11 +84,11 @@ public class ODEFunction implements ODEFunctionInterface {
 	private double[] forceMotion_X_Y_Z(PlanetBody planet){
 
 		double [] force = new double[3];
-		for (PlanetBody planetBody : planets) {
-			if (planet != planetBody) {
-				force[0] += ForceX_Between(planet, planetBody);
-				force[1] += ForceY_Between(planet, planetBody);
-				force[2] += ForceZ_Between(planet, planetBody);
+		for (int i = 0; i < planets.length; i++) {
+			if (planet != planets[i]) {
+				force[0] += ForceX_Between(planet, planets[i]);
+				force[1] += ForceY_Between(planet, planets[i]);
+				force[2] += ForceZ_Between(planet, planets[i]);
 			}
 		}
 
@@ -90,15 +96,45 @@ public class ODEFunction implements ODEFunctionInterface {
 	}
 
 	public static double ForceX_Between(PlanetBody one, PlanetBody other) {
+		double tempPow = Math.pow(one.getPosition().getX() - other.getPosition().getX(), 2);
+		double temp = Math.sqrt(tempPow);
+		double r = Math.pow(temp, 3);
 		return -(G * one.getM() * other.getM() *(one.getPosition().getX() - other.getPosition().getX())
-				/ Math.pow(Math.pow(one.getPosition().getX() - other.getPosition().getX(), 2), 3));
+				/ r);
 	}
 	public static double ForceY_Between(PlanetBody one, PlanetBody other) {
+		double tempPow = Math.pow(one.getPosition().getY() - other.getPosition().getY(), 2);
+		double temp = Math.sqrt(tempPow);
+		double r = Math.pow(temp, 3);
 		return -(G * one.getM() * other.getM() * (one.getPosition().getY() - other.getPosition().getY())
-				/ Math.pow(Math.pow(one.getPosition().getY() - other.getPosition().getY(), 2), 3));
+				/ r);
 	}
 	public static double ForceZ_Between(PlanetBody one, PlanetBody other) {
+		double tempPow = Math.pow(one.getPosition().getZ() - other.getPosition().getZ(), 2);
+		double temp = Math.sqrt(tempPow);
+		double r = Math.pow(temp, 3);
 		return -(G * one.getM() * other.getM() * (one.getPosition().getZ() - other.getPosition().getZ())
-				/ Math.pow(Math.pow(one.getPosition().getZ() - other.getPosition().getZ(), 2), 3));
+				/ r);
+	}
+
+	//Newton's second law of motion
+	private double[] motion_X_Y_Z(PlanetBody planet){
+
+		double[] motion = new double[3];
+
+		motion[0] = planet.getVelocity().getX()/planet.getM();
+		motion[1] = planet.getVelocity().getY()/planet.getM();
+		motion[2] = planet.getVelocity().getZ()/planet.getM();
+
+		return motion;
+	}
+
+	//Initial momenta
+	private void initialMomenta(){
+		for (int i = 0; i < planets.length; i++) {
+			System.out.println(planets[i].getPosition().toString());
+			planets[i].getPosition().mul(planets[i].getM());
+			System.out.println(planets[i].getPosition().toString());
+		}
 	}
 }
