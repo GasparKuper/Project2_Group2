4f35b9e2182db2da6d9b8d4a4077cddf52a81aa3
BMEJSchmitz <72268212+BMEJSchmitz@users.noreply.github.com>
2021-03-18 17:07:46 +0100

    Update UI.java

--- MAIN/UI.java
+++ MAIN/UI.java
@@ -1,7 +1,9 @@
-package MAIN;
 // Scale x,y,z  - 1:1*10^(-9)
 // Scale volume - 1:1*10^(-6)
 
+import MAIN.*;
+import MAIN.Body.*;
+import MAIN.Interfaces.*;
 import javafx.application.Application;
 import javafx.scene.*;
 import javafx.scene.input.KeyEvent;
@@ -17,7 +19,8 @@ public class UI extends Application{
 	public static final int HEIGHT = 750;
 
 	public void start(Stage primaryStage) throws Exception {
-
+		Initialize planets = new Initialize();
+		Vector3dInterface[][] init = planets.getPlanets();
 		Group solarSystem = new Group();
 		Scene scene = new Scene(solarSystem, WIDTH, HEIGHT);
 
@@ -29,47 +32,59 @@ public class UI extends Application{
 
 
 		Orbit sun = new Orbit(-0.6806783239281648, 1.080005533878725, 0.006564012751690170, 695.508);
-		sun.setLight("/Image/TexturSun.JPG");
+		sun.setLight("/Image/Textures/Sun.JPG");
+		sun.setState(init[0][0],init[0][1]);
 		solarSystem.getChildren().add(sun.getShape());
 
 		Orbit mercury = new Orbit(0.006047855986424127, -68.01800047868888, -5.702742359714534, 2.4397);
 		mercury.setImage("/Image/Textures/Mercury.JPG");
+		mercury.setState(init[1][0],init[1][1]);
 		solarSystem.getChildren().add(mercury.getShape());
+
 
 		Orbit venus = new Orbit(-94.35345478592035, 53.50359551033670, 6.131453014410347, 6.0518);
 		venus.setImage("/Image/Textures/Venus.JPG");
+		venus.setState(init[2][0],init[2][1]);
 		solarSystem.getChildren().add(venus.getShape());
 
 		Orbit earth = new Orbit(-147.1922101663588, -28.60995816266412, 0.008278183193596080, 6.371);
 		earth.setImage("/Image/Textures/Earth.JPG");
+		earth.setState(init[3][0],init[3][1]);
 		solarSystem.getChildren().add(earth.getShape());
 
 		Orbit moon = new Orbit(-147.2343904597218, -28.22578361503422, 0.01052790970065631, 1.7375);
 		moon.setImage("/Image/Textures/Moon.JPG");
+		moon.setState(init[4][0],init[4][1]);
 		solarSystem.getChildren().add(moon.getShape());
 
 		Orbit mars = new Orbit(-36.15638921529161, -216.7633037046744, -3.687670305939779, 3.3895);
 		mars.setImage("/Image/Textures/Mars.JPG");
+		mars.setState(init[5][0],init[5][1]);
 		solarSystem.getChildren().add(mars.getShape());
 
 		Orbit jupiter = new Orbit(178.1303138592153, -755.1118436250277, -0.8532838524802327, 69.911);
 		jupiter.setImage("/Image/Textures/Jupiter.JPG");
+		jupiter.setState(init[6][0],init[6][1]);
 		solarSystem.getChildren().add(jupiter.getShape());
 
 		Orbit saturn = new Orbit(632.8646641500651, -1358.172804527507, -1.578520137930810, 58.232);
 		saturn.setImage("/Image/Textures/Jupiter.JPG");
+		saturn.setState(init[7][0],init[7][1]);
 		solarSystem.getChildren().add(saturn.getShape());
 
 		Orbit titan = new Orbit(633.2873118527889, -1357.175556995868, -2.134637041453660, 2.5755);
 		titan.setImage("/Image/Textures/Titan.JPG");
+		titan.setState(init[8][0],init[8][1]);
 		solarSystem.getChildren().add(titan.getShape());
 
 		Orbit uranus = new Orbit(2395.195786685187, 1744.450959214586, -24.55116324031639, 25.362);
 		uranus.setImage("/Image/Textures/Uranus.JPG");
+		uranus.setState(init[9][0],init[9][1]);
 		solarSystem.getChildren().add(uranus.getShape());
 
 		Orbit neptune = new Orbit(4382.692942729203, -909.3501655486243, -82.27728929479486, 24.622);
 		neptune.setImage("/Image/Textures/Neptune.JPG");
+		neptune.setState(init[10][0],init[10][1]);
 		solarSystem.getChildren().add(neptune.getShape());
 
 		/*ImageView background = new ImageView("/Image/Textures/Space.JPG");
@@ -82,6 +97,9 @@ public class UI extends Application{
 		
 		PointLight lighting = new PointLight();
 		solarSystem.getChildren().add(lighting);
+
+		Thread animation = new Calculation(sun, mercury, venus, earth, moon, mars, jupiter, saturn, titan, uranus, neptune);
+		animation.start();
 
 		PerspectiveCamera camera = new PerspectiveCamera(true);
 		camera.setFieldOfView(25); // setting the camera to be telephoto
