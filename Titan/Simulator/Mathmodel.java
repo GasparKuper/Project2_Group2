c157d0d58d6c5cdefb2903cdf3824f5ec7c02768
Daniel Vandormael de Oliveira <56165721+danielvandormael@users.noreply.github.com>
2021-03-18 03:18:27 -0300

    Update Mathmodel.java

--- Titan/Simulator/Mathmodel.java
+++ Titan/Simulator/Mathmodel.java
@@ -2,20 +2,23 @@ package Titan;
 
 public class Mathmodel {
 
-   private static double stepsize = 863;
+   
+    private static double stepsize = 863;
     private static double maxtime = 3.154e+7;
     private static double radiusEarth = 6378000;
     private static double radiusTitan = 2574700;
     private static Vector3dInterface earthposition = new Vector3d(-1.471922101663588e+11,  -2.860995816266412e+10, 8.278183193596080e+06);
-    private static Vector3dInterface titanFinalposition = new Vector3d(1,1,1); //need to find position of titan after one year
+    public Vector3dInterface [] trajectoryTitan; //figure out how to get every position of titan
     public Vector3dInterface [] trajectorySolution;
+    public double stepsToSolution;
+
 
     public void bruteforce(){
         Boolean solution = false;
         Vector3dInterface[] trajectory;
         while(solution != true){
-            double angleX = Math.random() * 360;
-            double angleY = Math.random() * 180;
+            double angleX = Math.random() * 2*Math.PI;
+            double angleY = Math.random() * Math.PI;
 
             Vector3dInterface p0 = CoordInEarth(earthposition, radiusEarth, angleX, angleY);
             Vector3dInterface tip = CoordInEarth(earthposition, radiusEarth+1, angleX, angleY);
@@ -30,16 +33,16 @@ public class Mathmodel {
 
             Vector3dInterface v0 = VelocityVector(velocity, tip, p0);
             trajectory = p.trajectory(p0, v0, maxtime, stepsize);
-            solution = Score(trajectory, titanFinalposition);
+            solution = Score(trajectory, trajectoryTitan);
         }
         trajectorySolution = trajectory;
     }
 
         // angleX = [0,2pie) angleY = [0,pie)
     public Vector3dInterface CoordInEarth(Vector3dInterface origin, double radius, double angleX, double angleY){
-        double x = origin.getX()+radius*Math.sin(angleY)*Math.cos(angleX);
+        double x = origin.getX()+radius*Math.sin(angleX)*Math.cos(angleY);
         double y = origin.getY()+radius*Math.sin(angleX)*Math.sin(angleY);
-        double z = origin.getZ()+radius*Math.cos(angleY);
+        double z = origin.getZ()+radius*Math.cos(angleX);
         return new Vector3d(x,y,z);
     }
 
@@ -50,19 +53,18 @@ public class Mathmodel {
         return temp;
     }
 
-    public double[] angleToTitan(Vector3dInterface earthpositon, Vector3dInterface titanposition ){
-        double[] angles = new double[2];
-        return angles;
-    }
+
 
 
     public Boolean Score(Vector3dInterface [] trajectory, Vector3dInterface finalpositionTitan){
-        Vector3dInterface temp =trajectory[trajectory.length-1].sub(finalpositionTitan);
-        double temp1 = temp.norm();
-        if(temp1 <= radiusTitan){
-            return true;
-        }else {
-            return false;
+        for(int x = 1; x< trajectory.length; x++){
+            Vector3dInterface temp =trajectory[trajectory.length-1].sub(finalpositionTitan);
+            double temp1 = temp.norm();
+            if(temp1 <= radiusTitan){
+                stepsToSolution = x;
+                return true;
+            }
         }
+        return false;
     }
 }
