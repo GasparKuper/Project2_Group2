8783a8a334594dfc6a5fdf6d2ed70371a5236ff1
Daniel Vandormael de Oliveira <56165721+danielvandormael@users.noreply.github.com>
2021-03-18 00:31:43 -0300

    Update Mathmodel.java

--- Titan/Simulator/Mathmodel.java
+++ Titan/Simulator/Mathmodel.java
@@ -2,30 +2,67 @@ package Titan;
 
 public class Mathmodel {
 
+   private static double stepsize = 863;
+    private static double maxtime = 3.154e+7;
+    private static double radiusEarth = 6378000;
+    private static double radiusTitan = 2574700;
+    private static Vector3dInterface earthposition = new Vector3d(-1.471922101663588e+11,  -2.860995816266412e+10, 8.278183193596080e+06);
+    private static Vector3dInterface titanFinalposition = new Vector3d(1,1,1); //need to find position of titan after one year
+    public Vector3dInterface [] trajectorySolution;
+
     public void bruteforce(){
-        //iterate through time from lowest to highest possible time
-        //sim path of titan with t time
-        //get the coord of titan at t time
-        //find angles of direction vector
-        //find location in earth to match the direction vector
-        //iterate around the direction vector with a range of one unit degree higher or lower
-        //fire rocket with x speed from location
-        //get last position in trajectory
-        //evaluate position in relation to the position of titan
+        Boolean solution = false;
+        Vector3dInterface[] trajectory;
+        while(solution != true){
+            double angleX = Math.random() * 360;
+            double angleY = Math.random() * 180;
 
+            Vector3dInterface p0 = CoordInEarth(earthposition, radiusEarth, angleX, angleY);
+            Vector3dInterface tip = CoordInEarth(earthposition, radiusEarth+1, angleX, angleY);
 
+            double velocity = 0;
+            //minimum velocity to leave earth
+            while(velocity <= 17000){
+                velocity = Math.random()*60000;
+            }
+
+            ProbeSimulator p = new ProbeSimulator();
+
+            Vector3dInterface v0 = VelocityVector(velocity, tip, p0);
+            trajectory = p.trajectory(p0, v0, maxtime, stepsize);
+            solution = Score(trajectory, titanFinalposition);
+        }
+        trajectorySolution = trajectory;
     }
 
+        // angleX = [0,2pie) angleY = [0,pie)
     public Vector3dInterface CoordInEarth(Vector3dInterface origin, double radius, double angleX, double angleY){
         double x = origin.getX()+radius*Math.sin(angleY)*Math.cos(angleX);
         double y = origin.getY()+radius*Math.sin(angleX)*Math.sin(angleY);
-        double z = origin.getZ()+radius*Math.cos(angleX);
+        double z = origin.getZ()+radius*Math.cos(angleY);
         return new Vector3d(x,y,z);
     }
 
+    //tip is one meter above of tail so we know in which direction your vector will need to go
+    public Vector3dInterface VelocityVector(double speed, Vector3dInterface tip, Vector3dInterface tail){
+        Vector3dInterface temp = tip.sub(tail);
+        temp.mul(speed);
+        return temp;
+    }
 
-    public double Score(Vector3dInterface [] projection){
-        //score is distance between the probe final location and titan;
-        return 0.0;
+    public double[] angleToTitan(Vector3dInterface earthpositon, Vector3dInterface titanposition ){
+        double[] angles = new double[2];
+        return angles;
+    }
+
+
+    public Boolean Score(Vector3dInterface [] trajectory, Vector3dInterface finalpositionTitan){
+        Vector3dInterface temp =trajectory[trajectory.length-1].sub(finalpositionTitan);
+        double temp1 = temp.norm();
+        if(temp1 <= radiusTitan){
+            return true;
+        }else {
+            return false;
+        }
     }
 }
