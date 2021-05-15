d8b1abfe3d825941fefa2730fa8ded7ef5c264ac
GasparKuper <72268272+GasparKuper@users.noreply.github.com>
2021-05-15 18:33:54 +0200

    Time improvement

--- src/main/java/Constant/Constant.java
+++ src/main/java/Constant/Constant.java
@@ -1,6 +1,8 @@
 package Constant;
 
 import Body.Vector3d;
+import Experiments.StepSize;
+import Interfaces.Vector3dInterface;
 
 public class Constant {
 
@@ -15,20 +17,34 @@ public class Constant {
     public static int SOLVER = 3;
 
     /**
+     * Step size used to calculate trajectories
+     */
+    public static double STEPSIZE = 20000;
+
+    /**
+     * Final time used to calculate trajectories
+     */
+    public static double[] FINALTIME = {1.9628455E7,1.9640623E7,1.9630238E7,31556952};
+
+    /**
      * Array VELOCITIES contains the initial velocity of the probe computed by the brute force for each solver
      * The values inside the array are used to start the trajectory of the probe
      */
-    public static Vector3d[] VELOCITIES = {new Vector3d(26059.64822902804,-17025.274251441046,-551.8266716533692),
-                                           new Vector3d(26064.922970305346,-17015.98491274584,-560.1116360658285),
-                                           new Vector3d(26060.15681226142,-17025.499984899376,-551.6491230911201),
-                                           new Vector3d(26060.15681226142,-17025.499984899376,-551.6491230911201)};
+    public static Vector3dInterface[] VELOCITIES = {new Vector3d(45754.49514230483,-38810.39783267506,-527.8779302246595),
+                                           new Vector3d(45653.49832969943,-38930.537266394975,-411.5923649919469),
+                                           new Vector3d(45767.13717953718,-38795.42429374674,-532.7219902108546),
+                                           new Vector3d(45767.13717953718,-38795.42429374674,-532.7219902108546)};
 
     /**
      * Array TITANLASTPOS contains the result of the calculation of the last position of titan for each solver
      * The values inside the array are used by the brute force
      */
-    public static Vector3d[] TITANLASTPOS = {new Vector3d(8.790322792822222E11,-1.2037926536678767E12,-1.4406260567073011E10),
-                                             new Vector3d(8.792050124292278E11,-1.203411806119769E12,-1.4620174774740269E10),
-                                             new Vector3d(8.790389926264305E11,-1.2038031270354197E12,-1.4401675088069757E10),
+    public static Vector3d[] TITANLASTPOS = {new Vector3d(8.792064226203754E11,-1.2040466447405679E12,-1.4284761255510523E10),
+                                             new Vector3d(8.751845348489886E11,-1.2082007407347507E12,-1.1752216332025648E10),
+                                             new Vector3d(8.792207314653517E11,-1.2042094620928135E12,-1.421029948775191E10),
                                              new Vector3d(8.790389926264305E11,-1.2038031270354197E12,-1.4401675088069757E10)};
+    /**
+     * Start position of the probe
+     */
+    public static final Vector3d STARTPOS = new Vector3d(-1.4718861838613153E11, -2.8615219147677864E10, 8174296.311571818);
 }
--- src/main/java/Run/CalculationOutput.java
+++ src/main/java/Run/CalculationOutput.java
@@ -1,23 +1,22 @@
 package Run;
 
+import Body.State;
 import Body.Vector3d;
 import ODESolver.ProbeSimulator;
 
-import static Constant.Constant.SOLVER;
-import static Constant.Constant.VELOCITIES;
+import static Constant.Constant.*;
 
 public class CalculationOutput extends Thread{
-
+    private Vector3d[] trajectoryOfProbe;
+    private Body.State[] trajectoryOfAll;
     public void Solver() {
         ProbeSimulator simulator = new ProbeSimulator();
 
         //Array the trajectory of the probe
-        Vector3d[] trajectoryOfProbe = (Vector3d[]) simulator.trajectory(
-                new Vector3d(-1.4718861838613153E11, -2.8615219147677864E10 ,8174296.311571818),
-                VELOCITIES[SOLVER-1], 31556952, 360);
+        this.trajectoryOfProbe = (Vector3d[]) simulator.trajectory(STARTPOS,VELOCITIES[SOLVER-1], FINALTIME[SOLVER-1], STEPSIZE);
 
 
-        Body.State[] trajectoryOfAll = simulator.getTrajectory();
+        trajectoryOfAll = simulator.getTrajectory();
 
         if(SOLVER == 1)
             System.out.println("SYMPLECTIC EULER SOLVER");
@@ -33,5 +32,11 @@ public class CalculationOutput extends Thread{
         System.out.println("Titan = " + trajectoryOfAll[trajectoryOfAll.length-1].celestialBody.get(8).position);
         //Distance between Titan and the probe
         System.out.println("Distance between titan and the probe = " + trajectoryOfProbe[trajectoryOfProbe.length-1].dist(trajectoryOfAll[trajectoryOfAll.length-1].celestialBody.get(8).position));
+    }
+    public Vector3d getFinalPos(){
+        return this.trajectoryOfProbe[trajectoryOfProbe.length-1];
+    }
+    public Vector3d getTitanLastPos(){
+        return this.trajectoryOfAll[trajectoryOfAll.length-1].celestialBody.get(8).position;
     }
 }
