dea21176ca6c987647c6aedd3ec37d3412329f79
Dikaver <dika1596@gmail.com>
2021-05-17 15:52:33 +0300

    Find the best launch position on the Earth

--- src/main/java/Run/BruteForce.java
+++ src/main/java/Run/BruteForce.java
@@ -13,42 +13,8 @@ public class BruteForce {
     private Vector3d posBest;
     private Vector3d velBest;
     private double distance = Double.MAX_VALUE;
-    private int day;
-
-    public void shot(){
-        //List of velocity composition
-        int[] composVel = new int[(60000/500) * 2];
-        int tmp_Vel = -60000;
-        for (int i = 0; i < composVel.length; i++) {
-            composVel[i] = tmp_Vel;
-            tmp_Vel += 500;
-        }
-
-        Vector3d position = new Vector3d(-5796000.0,-2645000,0);
-
-
-        for (int i = 6; i < composVel.length; i++) {
-            for (int j = 0; j < composVel.length; j++) {
-                for (int k = 0; k < composVel.length; k++) {
-                    Vector3d velocity = new Vector3d(composVel[i], composVel[j], composVel[k]);
-
-                    if (velocity(velocity)) {
-                        tryShot(position, velocity);
-                    }
-                }
-            }
-        }
-    }
 
     public void BruteForceStart(){
-
-        //List of velocity composition
-        int[] composVel = new int[(60000/1000) * 2];
-        int tmp_Vel = -60000;
-        for (int i = 0; i < composVel.length; i++) {
-            composVel[i] = tmp_Vel;
-            tmp_Vel += 1000;
-        }
 
         //List of position composition
         int[] composPos = new int[(radiusEarth/23000)*6];
@@ -59,24 +25,13 @@ public class BruteForce {
         }
 
         //Set-Up X (Save your time)
-        for (int x = 587; x < composPos.length; x++) {
+        for (int x = 0; x < composPos.length; x++) {
             System.out.println("X = " + x);
             for (int y = 0; y < composPos.length; y++) {
                 for (int z = 0; z < composPos.length; z++) {
                     Vector3d position = new Vector3d(composPos[y], composPos[x], composPos[z]);
-
                     if (radius(position)) {
-                        for (int i = 0; i < composVel.length; i++) {
-                            for (int j = 0; j < composVel.length; j++) {
-                                for (int k = 0; k < composVel.length; k++) {
-                                    Vector3d velocity = new Vector3d(composVel[i], composVel[j], composVel[k]);
-
-                                    if (velocity(velocity)) {
-                                        tryShot(position, velocity);
-                                    }
-                                }
-                            }
-                        }
+                        tryShot(position, new Vector3d(0, 0, 0));
                     }
                 }
             }
@@ -85,7 +40,6 @@ public class BruteForce {
         System.out.println("The Best Shot");
         System.out.println("Position: " + posBest);
         System.out.println("Velocity: " + velBest);
-        System.out.println("Time: " + day );
         System.out.println("Distance: " + distance);
     }
 
@@ -103,16 +57,6 @@ public class BruteForce {
 
 
     /**
-     * Check the speed of the probe
-     * @param vel velocity of the probe
-     * @return true, if velocity between 50km/s and 60.1km/s
-     */
-    private boolean velocity(Vector3d vel){
-        double magnitude = vel.norm();
-        return (magnitude >= 50000.0 && magnitude <= 60001.0);
-    }
-
-    /**
      * Lucky shot
      * @param pos position of the probe
      * @param vel velocity of the probe
@@ -121,10 +65,9 @@ public class BruteForce {
         ProbeSimulator simulator = new ProbeSimulator();
 
         double day = 24*60*60;
-        double year = 365.25*day;
 
         //Array the trajectory of the probe
-        simulator.trajectory(pos, vel, year, day);
+        simulator.trajectory(pos, vel, day, day);
 
         Body.State[] trajectoryOfAll = simulator.getTrajectory();
 
@@ -138,13 +81,11 @@ public class BruteForce {
     private void compare(Body.State[] trajectory){
 
         //Check distance between the probe and the Titan
-        for (int i = 0; i < trajectory.length; i++) {
-            Vector3d probe = trajectory[i].celestialBody.get(11).getPosition();
-            Vector3d titan = trajectory[i].celestialBody.get(8).getPosition();
+            Vector3d probe = trajectory[0].celestialBody.get(11).getPosition();
+            Vector3d titan = new Vector3d(8.792207314653517E11,-1.2042094620928135E12,-1.421029948775191E10);
             double distance = probe.dist(titan) - radiusTitan;
             //Update the best shot
             if(distance < this.distance){
-                this.day = i;
                 this.distance = distance;
                 PlanetBody tmp_probe = trajectory[0].celestialBody.get(11);
                 PlanetBody tmp_earth = trajectory[0].celestialBody.get(3);
@@ -153,16 +94,12 @@ public class BruteForce {
                 System.out.println("===============================================================");
                 System.out.println("Position: " + posBest);
                 System.out.println("Velocity: " + velBest);
-                System.out.println("Time: " + day );
                 System.out.println("Distance: " + distance);
             }
-        }
     }
 
     public static void main(String[] args) {
         BruteForce run = new BruteForce();
-
-//        run.BruteForceStart();
-        run.shot();
+        run.BruteForceStart();
     }
 }
