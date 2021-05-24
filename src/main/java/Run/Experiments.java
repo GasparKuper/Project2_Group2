3d2e668881a88297e72abe48c530713404bdf7a1
Dikaver <dika1596@gmail.com>
2021-05-24 20:07:35 +0300

    Added comments

--- src/main/java/Run/Experiments.java
+++ src/main/java/Run/Experiments.java
@@ -7,17 +7,22 @@ import java.text.DecimalFormat;
 import static Constant.Constant.*;
 
 public class Experiments extends Thread{
-    private Body.State[] trajectoryOfAll;
+
+    /**
+     * Gives you experiments with each solver
+     */
     public void Solver() {
         ProbeSimulator simulator = new ProbeSimulator();
 
         double day = 24*60*60;
         double year = 365.25*day;
         double[] array = {day, 12*60*60, 60*60, 30*60, 10*60, 60};
+
         double[] earth1day = {-1.467017349489421E+08*1000, -3.113778902611655E+07*1000, 8.350045664343983E+03*1000};
         double[] earth1year = {-1.475975941838746E+08*1000, -2.884406357806718E+07*1000, 2.039237780630402E+04*1000};
-        //Array the trajectory of the probe
+
         DecimalFormat df = new DecimalFormat("#.########");
+
         for (int i = 0; i < 4; i++) {
             SOLVER = i+1;
             double best = Double.MAX_VALUE;
@@ -42,34 +47,61 @@ public class Experiments extends Thread{
                 double time = (double) endTime - startTime;
 
 
-                trajectoryOfAll = simulator.getTrajectory();
+                Body.State[] trajectoryOfAll = simulator.getTrajectory();
 
-                double tmpX = trajectoryOfAll[trajectoryOfAll.length - 1].celestialBody.get(3).position.getX() - earth1year[0];
-                double tmpY = trajectoryOfAll[trajectoryOfAll.length - 1].celestialBody.get(3).position.getY() - earth1year[1];
-                double tmpZ = trajectoryOfAll[trajectoryOfAll.length - 1].celestialBody.get(3).position.getZ() - earth1year[2];
-                double errorX = Math.abs(tmpX)/Math.abs(earth1year[0]);
-                double errorY = Math.abs(tmpY)/Math.abs(earth1year[1]);
-                double errorZ = Math.abs(tmpZ)/Math.abs(earth1year[2]);
+                //CHANGE ARRAY *CA, it depends on which time, you want check
+                //earth1year = 1 year 6 hours (NASA Coordinates)
+                //earth1day = 1 day (NASA Coordinates)
+                double tmpX = trajectoryOfAll[trajectoryOfAll.length - 1].celestialBody.get(3).position.getX() -
+                        earth1year[0]; //CA
+
+                double tmpY = trajectoryOfAll[trajectoryOfAll.length - 1].celestialBody.get(3).position.getY() -
+                        earth1year[1]; //CA
+
+                double tmpZ = trajectoryOfAll[trajectoryOfAll.length - 1].celestialBody.get(3).position.getZ() -
+                        earth1year[2]; //CA
+
+
+                //Calculate errors |p*-p| / |p|
+                double errorX = Math.abs(tmpX)/
+                        Math.abs(earth1year[0]); //CA
+
+                double errorY = Math.abs(tmpY)/
+                        Math.abs(earth1year[1]); //CA
+
+                double errorZ = Math.abs(tmpZ)/
+                        Math.abs(earth1year[2]); //CA
+
+
                 double average = (errorX + errorY + errorZ)/3;
                 System.out.println("Step size = " + array[j]);
+
+                //Outputs solvers coordinates
                 System.out.println(trajectoryOfAll[trajectoryOfAll.length - 1].celestialBody.get(3).position.getX());
                 System.out.println(trajectoryOfAll[trajectoryOfAll.length - 1].celestialBody.get(3).position.getX());
                 System.out.println(trajectoryOfAll[trajectoryOfAll.length - 1].celestialBody.get(3).position.getZ());
+
+                //Outputs errors
                 System.out.println("Average = " + df.format(average*100.0)+ "%");
                 System.out.println(df.format(errorX*100.0) + "%");
                 System.out.println(df.format(errorY*100.0)+ "%");
                 System.out.println(df.format(errorZ*100.0)+ "%");
+
+                //The best calculation
                 if(average < best) {
                     best = average;
                     stepBest = array[j];
                     timeX = time;
                 }
+
+                //The worst calculation
                 if(average > worst) {
                     worst = average;
                     stepWorst = array[j];
                     timeX1 = time;
                 }
             }
+            //Outputs the best and the worst calculation
             System.out.println("The best step = " + stepBest + " average = " + df.format(best*100.0) + "%" + " time = " + timeX + "ms");
             System.out.println("The worst step = " + stepWorst + " average = " + df.format(worst*100.0) + "%" + " time = "  + timeX1 + "ms");
         }
