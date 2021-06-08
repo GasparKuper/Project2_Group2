2dcd2acd66698fa4694f91b519089f8523c0c41c
Dikaver <dika1596@gmail.com>
2021-06-08 19:45:54 +0300

    Structured solvers

---
+++ src/main/java/Body/Solvers/RungeKutta.java
@@ -0,0 +1,58 @@
+package Body.Solvers;
+
+import Body.SpaceCrafts.State;
+import Interfaces.ODEFunctionInterface;
+import Interfaces.RateInterface;
+import Interfaces.StateInterface;
+
+/**
+ * 4rd order Runge-Kutta
+ */
+public class RungeKutta {
+
+    /**
+     * Update rule for one step.
+     *
+     * @param   f   the function defining the differential equation dy/dt=f(t,y)
+     * @param   t   the time
+     * @param   y   the state
+     * @param   h   the step size
+     * @return  the new state after taking one step
+     */
+    public StateInterface step(ODEFunctionInterface f, double t, StateInterface y, double h) {
+
+        RateInterface k1 = findK1(f,t,y);
+        RateInterface k2 = findK2(f,t,h,y,k1);
+        RateInterface k3 = findK3(f,t,h,y,k2);
+        RateInterface k4 = findK4(f,t,h,y,k3);
+
+        State state = ((State) y).clone();
+
+        state = (State) new ImplicitEuler().step(h/6, k1, state);
+        state = (State) new ImplicitEuler().step(2*h/6, k2, state);
+        state = (State) new ImplicitEuler().step(2*h/6, k3, state);
+        state = (State) new ImplicitEuler().step(h/6, k4, state);
+
+        return state;
+    }
+
+    public RateInterface findK1(ODEFunctionInterface f, double t, StateInterface y) {
+        State clone = ((State) y).clone();
+        return f.call(t, clone);
+    }
+
+    public RateInterface findK2(ODEFunctionInterface f, double t, double h, StateInterface y, RateInterface k1) {
+        State clone = ((State) y).clone();
+        return f.call(t + h/2, new SymplecticEuler().step(1.0/2, k1, clone));
+    }
+
+    public RateInterface findK3(ODEFunctionInterface f, double t, double h, StateInterface y, RateInterface k2) {
+        State clone = ((State) y).clone();
+        return f.call(t + h/2, new SymplecticEuler().step(1.0/2, k2, clone));
+    }
+
+    public RateInterface findK4(ODEFunctionInterface f, double t, double h, StateInterface y, RateInterface k3) {
+        State clone = ((State) y).clone();
+        return f.call(t + h, new SymplecticEuler().step(1.0, k3, clone));
+    }
+}
---
+++ src/main/java/Body/Solvers/SymplecticEuler.java
@@ -0,0 +1,46 @@
+package Body.Solvers;
+
+import Body.Planets.PlanetBody;
+import Body.SpaceCrafts.State;
+import Body.Vector.Vector3d;
+import Interfaces.RateInterface;
+import Interfaces.StateInterface;
+import ODESolver.Function.Rate;
+
+import java.util.LinkedList;
+
+public class SymplecticEuler {
+
+    /**
+     * SYMPLECTIC EULER
+     * Updates position and velocity of each planets
+     * @param step   The time-step of the update
+     * @param rate   Acceleration array.
+     * @return The new state after the update. Required to have the same class as 'this'.
+     */
+    public StateInterface step(double step, RateInterface rate, State state) {
+
+        LinkedList<Vector3d> acceleration = ((Rate) rate).getAcceleration();
+
+        if(acceleration == null)
+            throw new RuntimeException("Acceleration is empty");
+
+        for (int i = 0; i < state.celestialBody.size(); i++) {
+            PlanetBody planet = state.celestialBody.get(i);
+
+            //New Velocity = old Velocity + stepSize * Rate (Acceleration)
+            planet.setVelocity((Vector3d) planet.getVelocity().addMul(step, acceleration.get(i)));
+
+            //New position = old Position + stepSize * Velocity
+            planet.setPosition((Vector3d) planet.getPosition().addMul(step, planet.getVelocity()));
+
+            if(i == state.celestialBody.size()-1){
+                state.position = planet.getPosition();
+                state.velocity = planet.getVelocity();
+            }
+        }
+
+        return state;
+    }
+
+}
