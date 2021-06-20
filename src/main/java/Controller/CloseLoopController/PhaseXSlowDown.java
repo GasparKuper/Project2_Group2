ae4194fe71252b984e00e3c65ef0f29ec20745a0
Dikaver <dika1596@gmail.com>
2021-06-20 06:41:11 +0300

    Feedback controller

--- src/main/java/Controller/CloseLoopController/PhaseXSlowDown.java
+++ src/main/java/Controller/CloseLoopController/PhaseXSlowDown.java
@@ -1,18 +1,19 @@
-package Controller;
+package Controller.CloseLoopController;
 
 import Body.SpaceCrafts.Lander;
+import Controller.FuelCalculationLander;
+import Controller.Integration;
 
 import java.util.ArrayList;
 
-public class Phase2 {
+public class PhaseXSlowDown {
 
     /**
      *
      * @param state The state of the lander
-     * @param time time to reach the max Velocity for the lander
      * @return Trajectory of the lander
      */
-    public ArrayList<Lander> phase2(Lander state, double time, double step){
+    public ArrayList<Lander> phaseToSlowDownX(Lander state, double step){
 
 
         ArrayList<Lander> result = new ArrayList<>();
@@ -20,44 +21,15 @@ public class Phase2 {
 
         Integration update = new Integration();
         FuelCalculationLander fuel = new FuelCalculationLander();
-
-        //todo Speed up Velocity X
-        int point = 0;
-        for (double i = 0; i < time; i+=step) {
-            Lander lastState = result.get(point);
-            //Distance to x = 0
-            double distance = lastState.getPosition().getX();
-
-            // Vx_max = ||distance / time||
-            double Vx_max = 2.0 * Math.abs((distance/time));
-
-            //Sin
-            double angleLander = lastState.getRotation();
-            double degreeInRadians = Math.toRadians(angleLander);
-            double sin = Math.sin(degreeInRadians);
-
-            // Acceleration_x = Vx_max / time
-            double x_Acceleration = ((Vx_max / time));
-
-            //U = ||Acceleration_x / sin(theta)||
-            double u = Math.abs(x_Acceleration/sin);
-
-            Lander tmp = update.step(result.get(point++), u, 0, step);
-            result.add(tmp);
-
-            //Fuel
-            fuel.calculateFuel(result.get(point), step, u, 0);
-        }
-
         //todo Slow down Velocity X
 
-        Lander lastState = result.get(point);
+        Lander lastState = state;
         double distance = lastState.getPosition().getX();
         //Time to slow down the lander = ||distance / Vx_currentVelocity||
         double timeToSlowDown = Math.abs(distance/(lastState.getVelocity().getX() * 0.5));
 
         //Acceleration_x to slow down the lander = -Vx_currentVelocity / Time to slow down the lander
-        double x_Acceleration_ToSlowDown = (-lastState.getVelocity().getX()/timeToSlowDown) ;
+        double x_Acceleration_ToSlowDown = (lastState.getVelocity().getX()/timeToSlowDown) ;
 
         //Sin
         double angleLander = lastState.getRotation();
@@ -67,11 +39,9 @@ public class Phase2 {
         //U to slow down the lander = Acceleration_x to slow down the lander / sin(theta)
         double u_ToSlowDown = x_Acceleration_ToSlowDown/sin;
 
-        double iterationTime = timeToSlowDown;
 
-        double remainTime = timeToSlowDown % step;
-
-        for (double i = 0; i < iterationTime; i+=step) {
+        int point = 0;
+        while (timeToSlowDown >= step) {
             Lander tmp = update.step(result.get(point++), u_ToSlowDown, 0, step);
             result.add(tmp);
 
@@ -94,6 +64,8 @@ public class Phase2 {
             u_ToSlowDown = x_Acceleration_ToSlowDown/sin;
         }
 
+        double remainTime = timeToSlowDown % step;
+
         //Final Step
         x_Acceleration_ToSlowDown = -lastState.getVelocity().getX() / remainTime;
         u_ToSlowDown = Math.abs(x_Acceleration_ToSlowDown / sin);
@@ -104,5 +76,4 @@ public class Phase2 {
 
         return result;
     }
-
 }
